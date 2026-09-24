e8f5cb02f47b189e3835e22827d8c5b937625a23
Prathamesh <prathamesh.sh21@gmail.com>
2026-09-24 22:20:20 +0530

    "Configure frontend API for deployed backend"

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,11 +1,9 @@
-//centeralized API setup
-
 import axios from 'axios';
 import qs from 'qs';
 
 export const axiosInstance = axios.create({
-    baseURL: '/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
     withCredentials: true,
-    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
-})
-
+    paramsSerializer: params =>
+        qs.stringify(params, { arrayFormat: 'repeat' }),
+});
